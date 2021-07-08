7c18f284e38c9677c799566e2bac4b9f2891117e
Ivan Barrera <34744114+ivnbarrera@users.noreply.github.com>
2021-07-08 18:04:12 -0500

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ from utils import load_models, get_OD_results, parse_labels, \
     get_similar_pictures
 
 # For local testing
-from env_variables import set_env_variables
+#from env_variables import set_env_variables
 
 
 def predict_od_kpt_helper(img_np, img_pil, box_thresh=0.8):
